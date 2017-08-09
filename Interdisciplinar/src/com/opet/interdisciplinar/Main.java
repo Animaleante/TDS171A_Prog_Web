38be3f9481db111b22dfc6fe96164e8b9d811560
Diogo S Moraes <srto.dih@gmail.com>
2017-08-09 19:36:06 -0300

    Initial

---
+++ Interdisciplinar/src/com/opet/interdisciplinar/Main.java
@@ -0,0 +1,16 @@
+package com.opet.interdisciplinar;
+
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+        new Main();
+    }
+
+    public Main()
+    {
+        System.out.println("Iniciado");
+        System.out.println("Finalizado");
+    }
+}
