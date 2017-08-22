1e56a8319ff30c2384fa1fff73bf5fc1888d9c0d
Lab. Desenvolvimento <Lab. Desenvolvimento@172.31.2.76>
2017-08-21 21:20:27 -0300

    Aula08

---
+++ Aula_070817/src/com/tds171a/aula08/main/Aula08.java
@@ -0,0 +1,14 @@
+package com.tds171a.aula08.main;
+
+import com.tds171a.aula08.utilitario.Leitor;
+
+public class Aula08
+{
+
+    public static void main(String[] args)
+    {
+        String texto = Leitor.readString();
+        System.out.println(texto);
+    }
+
+}
---
+++ Aula_070817/src/com/tds171a/aula08/pessoa/Pessoa.java
@@ -0,0 +1,41 @@
+package com.tds171a.aula08.pessoa;
+
+import java.util.Date;
+
+public class Pessoa
+{
+    private String nome;
+    private String sexo;
+    private Date dataNascimento;
+
+    public Pessoa(String nome, String sexo, Date dataNascimento)
+    {
+        this.nome = nome;
+        this.sexo = sexo;
+        this.dataNascimento = dataNascimento;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public Date getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public void setDataNascimento(Date dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+}
