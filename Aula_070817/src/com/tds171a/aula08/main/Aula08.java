23fd642275ffdd76d1e9f2204d717d4aa7f5da78
Lab. Desenvolvimento <Lab. Desenvolvimento@172.31.1.51>
2017-08-23 20:36:12 -0300

    Bla

--- Aula_070817/src/com/tds171a/aula08/main/Aula08.java
+++ Aula_070817/src/com/tds171a/aula08/main/Aula08.java
@@ -11,21 +11,44 @@ public class Aula08
     /**
      * Atributo estatico e final, definindo número de pessoas na lista
      */
-    private final static int NUM_PESSOAS = 2;
+    private final static int NUM_PESSOAS = 100000;
 
     public static void main(String[] args)
     {
         ArrayList<Pessoa> listaPessoas = new ArrayList<Pessoa>();
 
+        long tStart = System.currentTimeMillis();
         for(int i = 0; i < NUM_PESSOAS; i++) {
-            listaPessoas.add(montarPessoa());
+            //listaPessoas.add(montarPessoa());
+            listaPessoas.add(new Pessoa("Bla", "M", new Date("01/01/1970")));
         }
+        long tEnd = System.currentTimeMillis();
 
+        System.out.println("add::Elapsed: " + (tEnd - tStart) + " milisseconds");
+
+        tStart = System.currentTimeMillis();
         for(Pessoa pessoa : listaPessoas) {
-            System.out.println("Nome: " + pessoa.getNome());
-            System.out.println("Sexo: " + pessoa.getSexo());
-            System.out.println("Data de Nascimento: " + pessoa.getDataNascimento());
+            //System.out.println("Nome: " + pessoa.getNome());
+            //System.out.println("Sexo: " + pessoa.getSexo());
+            //System.out.println("Data de Nascimento: " + pessoa.getDataNascimento());
         }
+        tEnd = System.currentTimeMillis();
+        System.out.println("list::Elapsed: " + (tEnd - tStart) + " milisseconds");
+
+        tStart = System.currentTimeMillis();
+        /*for (int i = 0; i < NUM_PESSOAS; i++)
+        {
+            listaPessoas.remove(0);
+        }*/
+        /*for(int i = NUM_PESSOAS - 1; i >= 0; i--) {
+            listaPessoas.remove(i);
+        }*/
+        while(listaPessoas.size() > 0) {
+            //listaPessoas.remove(0);
+            listaPessoas.remove(listaPessoas.size()-1);
+        }
+        tEnd = System.currentTimeMillis();
+        System.out.println("remove::Elapsed: " + (tEnd - tStart) + " milisseconds");
     }
 
     @SuppressWarnings("deprecation")
