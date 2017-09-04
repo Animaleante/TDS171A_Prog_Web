7f8b2024dae6ded443e38fc5588b2b49c0e9e465
Lab. Desenvolvimento <Lab. Desenvolvimento@172.31.1.50>
2017-09-04 20:05:22 -0300

    Criada classe de exception

--- Aula_070817/src/com/tds171a/aula14/ClasseA.java
+++ Aula_070817/src/com/tds171a/aula14/ClasseA.java
@@ -1,10 +1,13 @@
 package com.tds171a.aula14;
 
+import com.tds171a.aula14.exceptions.InvalidDividerException;
+
 public class ClasseA
 {
-    public double divisao(int i, int j) {
+    public double divisao(int i, int j) throws InvalidDividerException {
         if(j == 0) {
-            throw new IllegalArgumentException("Não é permitido dividendo 0.");
+            // throw new InvalidDividerException("Não é permitido divisor 0.");
+            throw new InvalidDividerException();
         }
 
         return i / j;
--- Aula_070817/src/com/tds171a/aula14/MainTeste.java
+++ Aula_070817/src/com/tds171a/aula14/MainTeste.java
@@ -1,5 +1,7 @@
 package com.tds171a.aula14;
 
+import com.tds171a.aula14.exceptions.InvalidDividerException;
+
 public class MainTeste
 {
     public static void main(String[] args)
@@ -7,7 +9,14 @@ public class MainTeste
         System.out.println("inicio");
 
         ClasseA a = new ClasseA();
-        System.out.println("Divisão: " + a.divisao(20, 0));
+        try
+        {
+            System.out.println("Divisão: " + a.divisao(20, 0));
+        }
+        catch (InvalidDividerException e)
+        {
+            e.printStackTrace();
+        }
 
         System.out.println("fim");
     }
